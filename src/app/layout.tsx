44f0f55027dfd66cdf84643e625dc53275fc651c
calebkeltner <40076345+calebkeltner@users.noreply.github.com>
2023-12-27 15:16:45 -0600

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
       <body className="flex min-h-full flex-col">
         <RootLayout>{children}</RootLayout>
+        <SpeedInsights />
+        < Analytics />
       </body>
     </html>
   )
